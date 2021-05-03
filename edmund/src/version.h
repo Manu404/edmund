7a6edebb956ae9528cd17afb75e8134a6f379086
eis <istace.emmanuel@hotmail.com>
2021-05-03 23:33:09 +0200

    update version

--- edmund/src/version.h
+++ edmund/src/version.h
@@ -1,13 +1,13 @@
 #pragma once
 #ifndef VERSION_INCLUDED
 #define VERSION_INCLUDED
-//
-// major - minor - build - state
-//
-// major => 1.0 once public, increment on breaking changes, manual
-// minor => increment on patch release, no breaking changes, manual
-// build => global increment, never reset, used for dev, automatic on build
 
-#define EDMUND_VERSION "0.0.45"
+//
+// major: 1.0 once public, increment on breaking changes, manual
+// minor: increment on patch release, no breaking changes, manual
+// build: global increment, reset on minor, used for dev, automatic on build, used to assert running elf identity, could be omitted durinng commit
+//
+
+#define EDMUND_VERSION "0.0.61"
 
 #endif
