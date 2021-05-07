ee5332d55d2a91c6852fe84f8ede47d6eed37b8f
Emmanuel Istace <istace.emmanuel@hotmail.com>
2021-05-08 00:04:22 +0200

    fix serial, add vscode settings backup

---
+++ scripts/env/backup_settings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -r ~/.config/Code/User/settings.json ./vscode/
+cp -r ~/.config/Code/User/keybindings.json ./vscode/
--- scripts/run_serial.sh
+++ scripts/run_serial.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo microcom -s 115200 -p /dev/ttyUSB0 -l ../output/serial.log
+sudo microcom -s 115200 -p /dev/ttyUSB0 -l ../build/serial.log
